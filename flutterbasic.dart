2b1fbc862272f59d7f22dc3f77163011febe67cb
Shivskiran S <51108446+shivakiran-s@users.noreply.github.com>
2021-11-15 13:12:41 +0530

    Update flutterbasic.dart
    body: img added

--- flutterbasic.dart
+++ flutterbasic.dart
@@ -1,68 +1,34 @@
 // ignore_for_file: avoid_print
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Firstapp());
+  runApp(MaterialApp(
+      home:Home() ));
 }
 
-// ignore: use_key_in_widget_constructors
-class Firstapp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // ignore: todo
-    // TODO: implement createState
-    // throw UnimplementedError();
-    return FirstAppState();
-  }
-}
-
-class FirstAppState extends State<Firstapp> {
-  var questionIndex = 0;
-  void answerQuestion() {
-    setState(() {
-      questionIndex = questionIndex + 1;
-    });
-    print(questionIndex);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // ignore: todo
-    // TODO: implement build
-
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-            // ignore: prefer_const_constructors
-            title: Text('SKSN'),
-            backgroundColor: Colors.red[400]),
-        body: Center(
-          child: Text(
-            'hello world',
-            style: TextStyle(
-                color: Colors.red[500],
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {  },
-        child: Text('cont.'),),
-      ),
-    );
-    // throw UnimplementedError();
-  }
-}
 
 class Home extends StatelessWidget {
   // const Home({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+        appBar: AppBar(
+            // ignore: prefer_const_constructors
+            title: Text('SKSN'),
+            backgroundColor: Colors.red[400]),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://images.unsplash.com/photo-1569887652102-59696fef1528?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=718&q=80'),
+//           image: AssetImage('assets/image1.jpeg'),
+          )
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {  },
+        child: Text('cont.'),),
+      );
   }
 }
